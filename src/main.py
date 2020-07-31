c3a20b9f61c7471b58713bdd32ec4958a3ed8202
Juan Tovar <jujotomo@gmail.com>
2020-07-31 13:56:08 -0700

    Solved by Juan José Tovar

--- src/main.py
+++ src/main.py
@@ -1,9 +1,12 @@
 # Resolve the problem!!
-
+import re
 
 def run():
     # Start coding here
-
-
+   with open('encoded.txt', 'r', encoding='utf-8') as f:
+       text = f.read()
+       secret_message = re.findall('[a-z]', text)
+       print(''.join(secret_message))
+       
 if __name__ == '__main__':
     run()
